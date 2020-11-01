37778956e8efb0d56a731e0fd389e62bc27061b3
mathgunz@gmail.com <mathgunz@gmail.com>
2020-11-01 19:36:14 -0300

    postgress

--- login/src/main/resources/schema.sql
+++ login/src/main/resources/schema.sql
@@ -1,55 +1,64 @@
-CREATE TABLE IF NOT EXISTS oauth_client_details (
+create table oauth_client_details (
   client_id VARCHAR(256) PRIMARY KEY,
   resource_ids VARCHAR(256),
-  client_secret VARCHAR(256) NOT NULL,
+  client_secret VARCHAR(256),
   scope VARCHAR(256),
   authorized_grant_types VARCHAR(256),
   web_server_redirect_uri VARCHAR(256),
   authorities VARCHAR(256),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
-  additional_information VARCHAR(4000),
+  additional_information VARCHAR(4096),
   autoapprove VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS oauth_client_token (
+create table oauth_client_token (
   token_id VARCHAR(256),
-  token BLOB,
-  authentication_id VARCHAR(256) PRIMARY KEY,
+  token bytea,
+  authentication_id VARCHAR(256),
   user_name VARCHAR(256),
   client_id VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS oauth_access_token (
+create table oauth_access_token (
   token_id VARCHAR(256),
-  token BLOB,
+  token bytea,
   authentication_id VARCHAR(256),
   user_name VARCHAR(256),
   client_id VARCHAR(256),
-  authentication BLOB,
+  authentication bytea,
   refresh_token VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS oauth_refresh_token (
+create table oauth_refresh_token (
   token_id VARCHAR(256),
-  token BLOB,
-  authentication BLOB
+  token bytea,
+  authentication bytea
 );
 
-CREATE TABLE IF NOT EXISTS oauth_code (
-  code VARCHAR(256), authentication BLOB
+create table oauth_code (
+  code VARCHAR(256), authentication bytea
 );
 
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(256) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  enabled TINYINT(1),
-  UNIQUE KEY unique_username(username)
+create table oauth_approvals (
+  userId VARCHAR(256),
+  clientId VARCHAR(256),
+  scope VARCHAR(256),
+  status VARCHAR(10),
+  expiresAt TIMESTAMP,
+  lastModifiedAt TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS authorities (
-  username VARCHAR(256) NOT NULL,
-  authority VARCHAR(256) NOT NULL,
-  PRIMARY KEY(username, authority)
+create table users(
+	username VARCHAR(50) not null primary key,
+	password VARCHAR(50) not null,
+	enabled boolean not null
 );
+
+create table authorities (
+	username VARCHAR(255) not null,
+	authority VARCHAR(255) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+ 
